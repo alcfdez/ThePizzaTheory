be6af7dbdaecb458efa7e2ea00307c0bf3008ba9
alcfdez <alcfdez@hotmail.com>
2022-11-10 12:28:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const harina="Harina integral de centeno;"
+const agua="Agua";
+const aguaTemplada="agua templada";
+const sal= "Sal";
+const levadura="Levadura";
+const azucar="Azúcar";
+const aceite="Aceite";
+const salsa="Salsa de tomate";
+const queso="Mozarella";
+const pepperoni="Pepperoni";
+const aceitunas="Aceitunas";
+const champiñones="Champiñones";
+
+let harinaPeso="400gr";
+let aguaPeso="100gr";
+let aguaPesoTemplada="100gr"
+let aceitePeso="50gr";
+let salPeso="1 cucharada de café";
+let levaduraPeso="16gr";
+let azucarPeso="1 cucharadita de café";
+
+function prepararLevadura(){
+    console.log("Mezclar el " + aguaTemplada + ", el " + azucar + " y la " + levadura + "." );
+
+}
+prepararLevadura()
+
+function prepararMasa1(){
+    console.log( "mezclar la "+ harina + "con el " + aceite + " y " + aguaPeso + " de " + agua + "." );
+}
